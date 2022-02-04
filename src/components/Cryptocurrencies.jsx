1bb3e236aaaa19b170a336661a92463f9685c64b
Steven Tardiff <tardiff.steven@gmail.com>
2022-02-04 08:10:01 -0500

    added details and etc

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -3,7 +3,7 @@ import millify from 'millify';
 import { Link } from 'react-router-dom';
 import {Card, Row, Col ,Input} from 'antd'
 
-import { useGetCryptosQuery } from '../../services/cryptoApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
  
 const Cryptocurrencies = ({simplified}) => {
   const count = simplified ? 10 : 100;
@@ -33,8 +33,8 @@ const Cryptocurrencies = ({simplified}) => {
 
       <Row gutter={[32,32]} className='crypto-card-container'>
         {cryptos?.map((currency)=> (
-          <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
+          <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.uuid}>
+            <Link to={`/crypto/${currency.uuid}`}>
               <Card 
                 title={`${currency.rank}. ${currency.name}`} 
                 extra={<img className='crypto-image' src={currency.iconUrl}/>}
